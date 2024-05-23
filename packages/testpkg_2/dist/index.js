6cd13c6967678d37dd1d82fb2423f3f64cd1412f
wyswill <www.1151373571@live.com>
2024-05-23 16:42:27 +0800

    rebuild

--- packages/testpkg_2/dist/index.js
+++ packages/testpkg_2/dist/index.js
@@ -2,5 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 function default_1() {
     console.log('[ thi is form test pkg_2 ] >');
+    console.log('[ change from pkg_2 ] >');
 }
 exports.default = default_1;
