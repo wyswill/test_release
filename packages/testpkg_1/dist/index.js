8360606a55e93d6678a300738ee1bec925721fdf
wyswill <www.1151373571@live.com>
2024-05-23 16:24:15 +0800

    init

---
+++ packages/testpkg_1/dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function default_1() {
+    console.log('this is from test pkg_1');
+}
+exports.default = default_1;
---
+++ packages/testpkg_2/dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function default_1() {
+    console.log('[ thi is form test pkg_2 ] >');
+}
+exports.default = default_1;
